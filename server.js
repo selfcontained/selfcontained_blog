559422d83849339aa97c07fe3ed8296f9be6bd2d
Brad Harris <bmharris@gmail.com>
2012-05-01 21:14:19 -0600

    tabs > spaces, duh

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require('./blog/blog.js')
 	.init(require('./blog.json'))
 	.load(__dirname+'/articles', function(err) {
-		if(err) return console.log(err);
+		if(err) return console.error(err);
 		this.createApp().listen(8001);
 	});
