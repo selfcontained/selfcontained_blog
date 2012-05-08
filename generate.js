676d5e062eb7231b29c33d3a9f3931c6d98d56a4
Brad Harris <bmharris@gmail.com>
2012-05-07 19:29:36 -0600

    pulling out flipflop code and adding it as an npm dependency

--- generate.js
+++
@@ -1,6 +0,0 @@
-require('./blog/blog.js')
-	.init(require('./blog.json'))
-	.load(__dirname+'/articles', function(err) {
-		if(err) return console.error(err);
-		this.generate(__dirname+'/public');
-	});
